d1732a3091cd0438d44848898c158bb1a37b1175
Dmitry Naimark <dmitrynaimark@gmail.com>
2019-07-21 09:30:05 +0300

    Cleanup IsEven solution

--- Ch3_Functions/Page55_IsEvenRecursive.js
+++ Ch3_Functions/Page55_IsEvenRecursive.js
@@ -1,15 +1,11 @@
 // ---------------------------------------------------
 // Zero is even, one is odd, for any other number N, its evenness is the same as N - 2.
 // Define a recursive function isEven corresponding to description.
-//
-// DN: It should also handle negative numbers, I think.
 // ---------------------------------------------------
 function isEven(x) {
-    return isEvenRecursively(Math.abs(x));
-}
-
-function isEvenRecursively(x) {
-    if (x === 0) {
+    if (x < 0) {
+        return isEven(Math.abs(x));
+    } else if (x === 0) {
         return true;
     } else if (x === 1) {
         return false;
@@ -22,10 +18,10 @@ function isEvenRecursively(x) {
 // ---------------------------------------------------
 //                    Test Cases
 // ---------------------------------------------------
-// Even
+// Odd
 console.log(isEven(7));
 console.log(isEven(-1));
-// Odd
+// Even
 console.log(isEven(8));
 console.log(isEven(-10));
 console.log(isEven(0));
