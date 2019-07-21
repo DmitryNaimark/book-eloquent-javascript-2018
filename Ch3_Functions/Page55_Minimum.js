7947230f15aafcd9c080f4c0f050057b54023a74
Dmitry Naimark <dmitrynaimark@gmail.com>
2019-07-21 09:11:13 +0300

    [Page 55] Add solution for "min" function

---
+++ Ch3_Functions/Page55_Minimum.js
@@ -0,0 +1,13 @@
+// ---------------------------------------------------
+// Write a function min that takes two arguments and returns their minimum.
+// ---------------------------------------------------
+function min(a, b) {
+    return (a < b) ? a : b;
+}
+
+
+// ---------------------------------------------------
+//                    Test Cases
+// ---------------------------------------------------
+console.log(min(2, 3));
+console.log(min(3, 2));
