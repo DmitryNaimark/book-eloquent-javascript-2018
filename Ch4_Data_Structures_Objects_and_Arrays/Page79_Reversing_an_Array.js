a0db0ce887b0ce4cf9220f6d572e68195af9fe40
Dmitry Naimark <dmitrynaimark@gmail.com>
2019-07-23 21:14:54 +0300

    [Page 79] Add "Reversing an Array" solution

---
+++ Ch4_Data_Structures_Objects_and_Arrays/Page79_Reversing_an_Array.js
@@ -0,0 +1,38 @@
+// ---------------------------------------------------
+// For this exercise, write two functions, reverseArray and reverseArrayInPlace.
+// The first, reverseArray, takes an array as argument and produces a new array that has the
+// same elements in the inverse order.
+//
+// The second, reverseArrayInPlace, does what the reverse method does:
+// it modifies the array given as argument by reversing its elements.
+// Neither may use the standard reverse method.
+// ---------------------------------------------------
+function reverseArray(arr) {
+    let arrReversed = Array(arr.length);
+    
+    for (let i = 0, j = arr.length - 1; i < arr.length; i++, j--) {
+        arrReversed[j] = arr[i];
+    }
+    
+    return arrReversed;
+}
+
+function reverseArrayInPlace(arr) {
+    for (let i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+
+// ---------------------------------------------------
+//                    Test Cases
+// ---------------------------------------------------
+// [5, 4, 3, 2, 1]
+console.log(reverseArray([1, 2, 3, 4, 5]));
+
+let arr = [1,2,3,4,5];
+reverseArrayInPlace(arr);
+// [5, 4, 3, 2, 1]
+console.log(arr);
