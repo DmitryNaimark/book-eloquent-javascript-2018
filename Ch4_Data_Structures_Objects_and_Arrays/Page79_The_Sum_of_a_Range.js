4bdfb62e889b89c32dc25f837ccd56753d845cf5
Dmitry Naimark <dmitrynaimark@gmail.com>
2019-07-25 12:51:50 +0300

    [Page 79] Improve "The Sum of a Range" solution by adding step parameter and implementing "conditionFunc" instead of two loops.

--- Ch4_Data_Structures_Objects_and_Arrays/Page79_The_Sum_of_a_Range.js
+++ Ch4_Data_Structures_Objects_and_Arrays/Page79_The_Sum_of_a_Range.js
@@ -11,13 +11,19 @@
 // The function call range(1, 10, 2) should return [1, 3, 5, 7, 9].
 // Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 // ---------------------------------------------------
-function range(start, end) {
-    if (start > end) {
+// Note: it's possible to make calculations for default parameter.
+function range(start, end, step = start < end ? 1 : -1) {
+    if (step === 0) {
         return [];
     }
     
     let res = [];
-    for (let i = start; i <= end; i++) {
+    // Instead of doing 2 loops(when step > 0 and step < 0), we can create condition function depending on step.
+    let conditionFunc = step > 0
+        ? (i) => i <= end
+        : (i) => i >= end;
+    
+    for (let i = start; conditionFunc(i); i += step) {
         res.push(i);
     }
     
@@ -25,11 +31,36 @@ function range(start, end) {
 }
 
 function sum(arr) {
-    return arr.reduce((total, x) => total + x);
+    return arr.reduce((total, x) => total + x, 0);
 }
 
 
 // ---------------------------------------------------
 //                    Test Cases
 // ---------------------------------------------------
+// 15
+console.log(sum([1, 2, 3, 4, 5]));
+
+// [0, 2]
+console.log(range(0, 3, 2));
+// [5, 4, 3, 2]
+console.log(range(5, 2, -1));
+
+// 55
 console.log(sum(range(1, 10)));
+// 6
+console.log(sum(range(0, 3)));
+// 6
+console.log(sum(range(1, 3)));
+// 0
+console.log(sum(range(0, 0)));
+// 0
+console.log(sum(range(0, 0, 0)));
+// 0
+console.log(sum(range(10, 0, 1)));
+// 55
+console.log(sum(range(10, 0, -1)));
+// 0
+console.log(sum(range(0, 10, -1)));
+// -55
+console.log(sum(range(0, -10, -1)));
